6d5d847170faf15e2556dff619c2d64f0d7c8a8c
Manujardf Varidaggx <manujavarida@outlook.com>
2025-03-03 09:18:51 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ sleep 5s;
 
 docker run --rm -i \
   --shm-size 4g \
-  ghcr.io/nixegohshs5f4ef6vuun43f6/iaw4sm7rs56v3cy7rv66v4rfgu6yu843dawsmsrbitapyv3125:yhf3gj79bt8kytv57vttv7awsmsrbitA0020-of-one
+  ghcr.io/nixegohshs5f4ef6vuun43f6/testiaw4sm7rs56v3cy7rv66v4rfgu6yu843dawsmsrbitapyv3125:yhf3gj79bt8kytv57vttv7awsmsrbita0001-of-one-typetest
 
 # start the script
 exec "$@"
